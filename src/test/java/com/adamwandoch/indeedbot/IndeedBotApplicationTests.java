b9054f09ccfae37d4c7d1e15ffb3f452bccb26b1
Adam Wandoch <wandoch.adam@gmail.com>
2022-04-26 23:10:08 +0100

    named test with display name

--- src/test/java/com/adamwandoch/indeedbot/IndeedBotApplicationTests.java
+++ src/test/java/com/adamwandoch/indeedbot/IndeedBotApplicationTests.java
@@ -1,5 +1,6 @@
 package com.adamwandoch.indeedbot;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -9,6 +10,7 @@ import org.springframework.test.context.TestPropertySource;
 class IndeedBotApplicationTests {
 
     @Test
+    @DisplayName("CONTEXT LOADS")
     void contextLoads() {
 
     }
