777cca6e96db1299c67353a00cacf4f1f621a4ec
Adam Wandoch <wandoch.adam@gmail.com>
2022-04-27 22:21:04 +0100

    updated bot instance list for pinging

--- src/main/java/com/adamwandoch/indeedbot/PingController.java
+++ src/main/java/com/adamwandoch/indeedbot/PingController.java
@@ -26,7 +26,22 @@ public class PingController {
                                                  "http://indeedbotcalifornia-env.eba-mhqpsqdy.us-west-1.elasticbeanstalk.com/ping",
                                                  "http://indeedbotoregon-env.eba-daaqgedz.us-west-2.elasticbeanstalk.com/ping",
                                                  "http://indeedbottokyo-env.eba-xmsejbh8.ap-northeast-1.elasticbeanstalk.com/ping",
-                                                 "http://indeedbotireland-env.eba-cckhqftf.eu-west-1.elasticbeanstalk.com/ping"
+                                                 "http://indeedbotireland-env.eba-cckhqftf.eu-west-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotcapetown-env.eba-md74kpmh.af-south-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbothongkong-env.eba-swjz6jbt.ap-east-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotmumbai-env.eba-k83bwiia.ap-south-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotosaka-env.eba-7cpbujbn.ap-northeast-3.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotseoul-env.eba-e5uicp37.ap-northeast-2.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotsingapore-env.eba-5m52rter.ap-southeast-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotsydney-env.eba-rp7xuxxm.ap-southeast-2.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotcanada-env.eba-jxu6x6ue.ca-central-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotfrankfurt-env.eba-qpbqcwxw.eu-central-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotlondon-env.eba-cevpj4x9.eu-west-2.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotmilan-env.eba-emvxupbh.eu-south-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotparis-env.eba-qdcmu7p2.eu-west-3.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotstockholm-env.eba-y6zw9bvv.eu-north-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotbahrain-env.eba-tzhqvrds.me-south-1.elasticbeanstalk.com/ping",
+                                                 "http://indeedbotsaopaulo-env.eba-3kg9h3bq.sa-east-1.elasticbeanstalk.com/ping"
             );
 
     @GetMapping("/ping")
