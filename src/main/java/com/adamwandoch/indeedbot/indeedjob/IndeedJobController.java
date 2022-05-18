7066b2f7315b91aea010a0b6738b54bcadd20051
Adam Wandoch <wandoch.adam@gmail.com>
2022-05-18 17:49:37 +0100

    updated controller to include cors header in all responses

--- src/main/java/com/adamwandoch/indeedbot/indeedjob/IndeedJobController.java
+++ src/main/java/com/adamwandoch/indeedbot/indeedjob/IndeedJobController.java
@@ -3,6 +3,7 @@ package com.adamwandoch.indeedbot.indeedjob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin
 public class IndeedJobController {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndeedJobController.class);
